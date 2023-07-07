8f4cf6644f5c343aff2ffd1763d7a208c024a4b5
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-07 22:06:18 +0100

    feat:added _BackdropTitle and modfied other widgets

--- shrine/lib/backdrop.dart
+++ shrine/lib/backdrop.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:shrine/login.dart';
 import 'package:shrine/model/product.dart';
+import 'login.dart';
 
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
@@ -28,15 +30,67 @@ class _BackdropState extends State<Backdrop>
 
   late AnimationController _controller;
 
+  double _kFlingVelocity = 2.0;
 
-  
+  @override
+  initState() {
+    _controller = AnimationController(
+        vsync: this, value: 1.0, duration: Duration(milliseconds: 300));
+    super.initState();
+  }
 
-  _buildStack() {
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant Backdrop oldWidget) {
+    if (widget.currentCategory != oldWidget.currentCategory) {
+      _toggleBackdropLayerVisibility();
+    } else if (!_frontLayerVisible) {
+      _controller.fling(velocity: _kFlingVelocity);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  bool get _frontLayerVisible {
+    AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+        velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity);
+  }
+
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const layerTitleHeight = 48.0;
+    Size layerSize = constraints.biggest;
+
+    double layerTop = layerSize.height - layerTitleHeight;
+
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+            begin: RelativeRect.fromLTRB(
+                0.0, layerTop, 0.0, layerTop - layerSize.height),
+            end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0))
+        .animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
-        widget.backLayer,
-        _FrontLayer(child: widget.frontLayer)
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
+        PositionedTransition(
+            rect: layerAnimation,
+            child: _FrontLayer(
+              child: widget.frontLayer,
+              onTap: _toggleBackdropLayerVisibility,
+            ))
       ],
     );
   }
@@ -46,9 +100,29 @@ class _BackdropState extends State<Backdrop>
     var appBar = AppBar(
       elevation: 0.0,
       titleSpacing: 0.0,
-      leading: Icon(Icons.menu),
-      title: Text("SHRINE"),
+      // leading: IconButton(
+      //   icon: const Icon(Icons.menu),
+      //   onPressed: _toggleBackdropLayerVisibility,
+      // ),
+      title: _BackdropTitle(
+        listenable: _controller.view,
+        backTitle: widget.backTitle,
+        frontTitle: widget.frontTitle,
+        onPress: _toggleBackdropLayerVisibility,
+      ),
       actions: <Widget>[
+        IconButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => LoginPage()));
+          },
+          icon: Icon(
+            Icons.login,
+            semanticLabel: 'login',
+          ),
+        ),
         IconButton(
           onPressed: () {},
           icon: Icon(
@@ -65,13 +139,14 @@ class _BackdropState extends State<Backdrop>
         ),
       ],
     );
-    return Scaffold(appBar: appBar, body: _buildStack());
+    return Scaffold(appBar: appBar, body: LayoutBuilder(builder: _buildStack));
   }
 }
 
 class _FrontLayer extends StatelessWidget {
-  const _FrontLayer({super.key, required this.child});
+  const _FrontLayer({super.key, this.onTap, required this.child});
 
+  final VoidCallback? onTap;
   final Widget child;
 
   @override
@@ -83,11 +158,91 @@ class _FrontLayer extends StatelessWidget {
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            // GestureDetector(
-            //   child:
+            GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: onTap,
+                child: Container(
+                  height: 40.0,
+                  alignment: AlignmentDirectional.centerStart,
+                )),
             Expanded(child: child),
-            // )
           ]),
     );
+  }
+}
+
+class _BackdropTitle extends AnimatedWidget {
+  final void Function() onPress;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  _BackdropTitle({
+    Key? key,
+    required Animation<double> listenable,
+    required this.onPress,
+    required this.frontTitle,
+    required this.backTitle,
+  })  : _listenable = listenable,
+        super(key: key, listenable: listenable);
+
+  final Animation<double> _listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = _listenable;
+
+    return DefaultTextStyle(
+        style: Theme.of(context).textTheme.titleLarge!,
+        softWrap: false,
+        overflow: TextOverflow.ellipsis,
+        child: Row(
+          children: <Widget>[
+            SizedBox(
+                width: 72.0,
+                child: IconButton(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  onPressed: onPress,
+                  icon: Stack(children: <Widget>[
+                    Opacity(
+                        opacity: animation.value,
+                        child: const ImageIcon(
+                            AssetImage('assets/slanted_menu.png'))),
+                    FractionalTranslation(
+                      translation: Tween<Offset>(
+                              begin: Offset.zero, end: const Offset(1.0, 0.0))
+                          .evaluate(animation),
+                      child: const ImageIcon(AssetImage('assets/diamond.png')),
+                    )
+                  ]),
+                )),
+            Stack(
+              children: <Widget>[
+                Opacity(
+                  opacity: CurvedAnimation(
+                          parent: ReverseAnimation(animation),
+                          curve: const Interval(0.5, 1.0))
+                      .value,
+                  child: FractionalTranslation(
+                    translation: Tween<Offset>(
+                            begin: Offset.zero, end: const Offset(0.5, 0.0))
+                        .evaluate(animation),
+                    child: backTitle,
+                  ),
+                ),
+                Opacity(
+                  opacity: CurvedAnimation(
+                          parent: animation, curve: Interval(0.5, 1.0))
+                      .value,
+                  child: FractionalTranslation(
+                    translation: Tween<Offset>(
+                            begin: Offset(-0.25, 0.0), end: Offset.zero)
+                        .evaluate(animation),
+                    child: frontTitle,
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
