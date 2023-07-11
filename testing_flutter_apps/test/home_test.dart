61f93cef9c7094a5f6a7c9adda2882b4c31309d0
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-11 03:05:53 +0100

    update:modified test

--- testing_flutter_apps/test/home_test.dart
+++ testing_flutter_apps/test/home_test.dart
@@ -20,9 +20,24 @@ void main() {
       expect(find.text('Item 0'), findsOneWidget);
       await tester.fling(find.byType(ListView), const Offset(0, -200), 3000);
       await tester.pumpAndSettle();
-      expect(find.text(('Item 0')),findsNothing);
+      expect(find.text(('Item 0')), findsNothing);
     });
   });
-  
+  testWidgets('Testing IconButtons', (tester) async {
+    await tester.pumpWidget(createHomeScreen());
+    
+    expect(find.byIcon(Icons.favorite), findsNothing);
 
+    await tester.tap(find.byIcon(Icons.favorite_border).first);
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+
+    expect(find.text('Added to favorites'), findsOneWidget);
+    expect(find.byIcon(Icons.favorite), findsWidgets);
+
+    await tester.tap(find.byIcon(Icons.favorite).first);
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+
+    expect(find.text('Removed from favorites.'), findsOneWidget);
+    expect(find.byIcon(Icons.favorite), findsNothing);
+  });
 }
