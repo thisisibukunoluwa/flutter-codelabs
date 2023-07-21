e2f2f1a620298eb7f1edf6261b9798ec36df08ad
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-21 22:57:11 +0100

    feat:added src folder with files

---
+++ webview_in_flutter/lib/src/navigation_controls.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class NavigationControls extends StatelessWidget {
+   NavigationControls({required this.controller, super.key});
+
+  late final WebViewController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () async {
+            final messenger = ScaffoldMessenger.of(context);
+            if (await controller.canGoBack()) {
+              controller.goBack();
+            } else {
+              messenger.showSnackBar(
+                  const SnackBar(content: Text('No back history item')));
+            }
+            return;
+          },
+        ),
+        IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () async {
+              final messenger = ScaffoldMessenger.of(context);
+              if (await controller.canGoForward()) {
+                controller.goForward();
+              } else {
+                messenger.showSnackBar(
+                    const SnackBar(content: Text('No Forward history item'))
+                );
+              }
+              return;
+            }),
+        // IconButton(
+        //   icon: const Icon(Icons.replay),
+
+        //   onPressed: () {
+        //     final messenger = ScaffoldMessenger.of(context);
+        //     controller.reload();
+        //     messenger.showSnackBar(
+        //         const SnackBar(content: Text('Reloading.....')));
+        //   },
+        // ),
+      ],
+    );
+  }
+}
---
+++ webview_in_flutter/lib/src/web_view_stack.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewStack extends StatefulWidget {
+  const WebViewStack({super.key});
+
+  @override
+  State<WebViewStack> createState() => _WebViewStackState();
+}
+
+class _WebViewStackState extends State<WebViewStack> {
+  var loadingPercentage = 0;
+  Uri uri = Uri.parse('https://flutter.dev');
+  late final Future<WebViewController> _initWebViewFuture;
+  late final WebViewController controller;
+  late final NavigationDelegate _navigationDelegate;
+
+  @override
+  void initState() {
+    super.initState();
+    _initWebViewFuture = _initWebView();
+  }
+
+  Future<WebViewController> _initWebView() async {
+    _navigationDelegate = NavigationDelegate(
+      onPageStarted: (url) {
+        setState(() {
+          loadingPercentage = 0;
+        });
+      },
+      onProgress: (progress) {
+        setState(() {
+          loadingPercentage = progress;
+        });
+      },
+      onPageFinished: (progress) async {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      },
+      onNavigationRequest: (navigation) {
+        final host = Uri.parse(navigation.url).host;
+        if (host.contains('youtube.com')) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Blocking navigation to $host')));
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      },
+    );
+    controller = WebViewController();
+
+    await controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+    
+    await controller.addJavaScriptChannel('SnackBar',
+        onMessageReceived: (message) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(message.message)));
+    });
+    await controller.setNavigationDelegate(_navigationDelegate);
+
+    await controller.loadRequest(uri);
+
+    return Future.value(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(),
+      body: FutureBuilder<WebViewController>(
+        future: _initWebViewFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return WebViewWidget(controller: controller);
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Webview Initialization error'));
+          } else {
+            return Center(
+              child: CircularProgressIndicator(
+                value: loadingPercentage / 100.0,
+              ),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await controller.clearCache();
+          await controller.loadRequest(uri);
+        },
+       child:Icon(Icons.refresh)
+      ),
+    );
+  }
+}
