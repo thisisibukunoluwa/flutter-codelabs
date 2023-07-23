e1d0b72e6f179858ef176f2918805d3af3ff00b4
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-23 17:26:09 +0100

    feat:added HTML string and switch cases for loading flutter Assets and html strings , and the methods

--- webview_in_flutter/lib/src/menu.dart
+++ webview_in_flutter/lib/src/menu.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
+
+const String kExamplePage = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load file or HTML string example</title>
+</head>
+<body>
+
+<h1>Local demo page</h1>
+<p>
+ This is an example page used to demonstrate how to load a local file or HTML
+ string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
+ webview</a> plugin.
+</p>
+
+</body>
+</html>
+''';
+
 enum _MenuOptions {
   navigationDelegate,
   userAgent,
@@ -12,6 +32,9 @@ enum _MenuOptions {
   addCookie,
   setCookie,
   removeCookie,
+  loadFlutterAsset,
+  loadLocalFile,
+  loadHtmlString
 }
 
 class Menu extends StatefulWidget {
@@ -24,9 +47,116 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
-  final WebViewCookieManager cookieManager = WebViewCookieManager();
+  final cookieManager = WebViewCookieManager();
 
-  Future<void> _onListCookies(WebViewController controller) async {
+  
+  // Fu
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<_MenuOptions>(
+        onSelected: (value) async {
+          switch (value) {
+            case _MenuOptions.navigationDelegate:
+              await widget.controller
+                  .loadRequest(Uri.parse('https://youtube.com'));
+            case _MenuOptions.userAgent:
+              final userAgent = await widget.controller
+                  .runJavaScriptReturningResult('navigator.userAgent');
+              if (!mounted) return;
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text('$userAgent')));
+
+            case _MenuOptions.javascriptChannel:
+              await widget.controller.runJavaScript('''
+              var req = new XMLHttpRequest();
+              req.open('GET', "https://api.ipify.org/?format=json");
+              req.onload = function() {
+                if (req.status == 200) {
+                  let response = JSON.parse(req.responseText);
+                  SnackBar.postMessage("IP Address: " + response.ip);
+                } else {
+                  SnackBar.postMessage("Error: " + req.status);
+                }
+              }
+              req.send();''');
+
+            case _MenuOptions.clearCookies:
+              await _onClearCookies();
+
+            case _MenuOptions.listCookies:
+              await _onListCookies(widget.controller);
+
+            case _MenuOptions.addCookie:
+              await _onAddCookie(widget.controller);
+
+            case _MenuOptions.setCookie:
+              await _onSetCookie(widget.controller);
+
+            case _MenuOptions.removeCookie:
+              await _onRemoveCookie(widget.controller);
+
+            case _MenuOptions.loadFlutterAsset:
+              await _onLoadFlutterAssetExample(widget.controller, context);
+            if (!mounted) return;
+            case _MenuOptions.loadLocalFile:
+              await _onLoadLocalFileExample(widget.controller, context);
+            if (!mounted) return;
+            case _MenuOptions.loadFlutterAsset:
+              await _onLoadFlutterAssetExample(widget.controller, context);
+            if (!mounted) return;
+            case _MenuOptions.loadHtmlString:
+              await _onLoadHtmlStringExample(widget.controller, context);
+          }
+        },
+        itemBuilder: (context) => [
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.navigationDelegate,
+                child: Text('Navigate to youtube'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                  value: _MenuOptions.userAgent,
+                  child: Text('Show user-agent')),
+              const PopupMenuItem<_MenuOptions>(
+                  value: _MenuOptions.javascriptChannel,
+                  child: Text('Lookup IP Address')),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.clearCookies,
+                child: Text('Clear cookies'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.listCookies,
+                child: Text('List cookies'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.addCookie,
+                child: Text('Add cookie'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.setCookie,
+                child: Text('Set cookie'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.removeCookie,
+                child: Text('Remove cookie'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.loadFlutterAsset,
+                child: Text('Load Flutter Asset'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.loadHtmlString,
+                child: Text('Load HTML string'),
+              ),
+              const PopupMenuItem<_MenuOptions>(
+                value: _MenuOptions.loadLocalFile,
+                child: Text('Load local file'),
+             ),
+         ]
+    );
+  }
+
+
+Future<void> _onListCookies(WebViewController controller) async {
     final String cookies = await controller
         .runJavaScriptReturningResult('document.cookie') as String;
     if (!mounted) return;
@@ -83,88 +213,32 @@ class _MenuState extends State<Menu> {
     );
   }
 
-  //loading a flutter asset
+//loading a flutter asset
   Future<void> _onLoadFlutterAssetExample(
       WebViewController controller, BuildContext context) async {
     await controller.loadFlutterAsset('assets/www/index.html');
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return PopupMenuButton<_MenuOptions>(
-        onSelected: (value) async {
-          switch (value) {
-            case _MenuOptions.navigationDelegate:
-              await widget.controller
-                  .loadRequest(Uri.parse('https://twitter.com'));
-              break;
-            case _MenuOptions.userAgent:
-              final userAgent = await widget.controller
-                  .runJavaScriptReturningResult('navigator.userAgent');
-              if (!mounted) return;
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text('$userAgent')));
-              break;
-            case _MenuOptions.javascriptChannel:
-              await widget.controller.runJavaScript('''
-              var req = new XMLHttpRequest();
-              req.open('GET', "https://api.ipify.org/?format=json");
-              req.onload = function() {
-                if (req.status == 200) {
-                  let response = JSON.parse(req.responseText);
-                  SnackBar.postMessage("IP Address: " + response.ip);
-                } else {
-                  SnackBar.postMessage("Error: " + req.status);
-                }
-              }
-              req.send();''');
-              break;
-            case _MenuOptions.clearCookies:
-              await _onClearCookies();
-              break;
-            case _MenuOptions.listCookies:
-              await _onListCookies(widget.controller);
-              break;
-            case _MenuOptions.addCookie:
-              await _onAddCookie(widget.controller);
-            case _MenuOptions.setCookie:
-              await _onSetCookie(widget.controller);
-            case _MenuOptions.removeCookie:
-              await _onRemoveCookie(widget.controller);
-              break;
-          }
-        },
-        itemBuilder: (context) => [
-              const PopupMenuItem<_MenuOptions>(
-                value: _MenuOptions.navigationDelegate,
-                child: Text('Navigate to youtube'),
-              ),
-              const PopupMenuItem<_MenuOptions>(
-                  value: _MenuOptions.userAgent,
-                  child: Text('Show user-agent')),
-              const PopupMenuItem<_MenuOptions>(
-                  value: _MenuOptions.javascriptChannel,
-                  child: Text('Lookup IP Address')),
-              const PopupMenuItem<_MenuOptions>(
-                value: _MenuOptions.clearCookies,
-                child: Text('Clear cookies'),
-              ),
-              const PopupMenuItem<_MenuOptions>(
-                value: _MenuOptions.listCookies,
-                child: Text('List cookies'),
-              ),
-              const PopupMenuItem<_MenuOptions>(
-                value: _MenuOptions.addCookie,
-                child: Text('Add cookie'),
-              ),
-              const PopupMenuItem<_MenuOptions>(
-                value: _MenuOptions.setCookie,
-                child: Text('Set cookie'),
-              ),
-              const PopupMenuItem<_MenuOptions>(
-                value: _MenuOptions.removeCookie,
-                child: Text('Remove cookie'),
-              )
-            ]);
+  Future<void> _onLoadLocalFileExample(
+      WebViewController controller, BuildContext context) async {
+    final String pathToIndex = await _prepareLocalFile();
+    await controller.loadFile(pathToIndex);
+  }
+
+  Future<String> _prepareLocalFile() async {
+    final String tmpDir = (await getTemporaryDirectory()).path;
+    final File indexFile = File('$tmpDir/www/index.html');
+
+    await Directory('$tmpDir/www').create(recursive: true);
+
+    await indexFile.writeAsString(kExamplePage);
+
+    return indexFile.path;
+  }
+
+  Future<void> _onLoadHtmlStringExample(
+      WebViewController controller, BuildContext context) async {
+    await controller.loadHtmlString(kExamplePage);
   }
 }
+
